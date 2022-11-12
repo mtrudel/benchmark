84c37b8d8b8b018529f8d93dc2daf149d0e547c5
Mat Trudel <mat@geeky.net>
2022-11-12 13:46:48 -0500

    Initial commit of mix new benchmark

---
+++ lib/benchmark.ex
@@ -0,0 +1,18 @@
+defmodule Benchmark do
+  @moduledoc """
+  Documentation for `Benchmark`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Benchmark.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
